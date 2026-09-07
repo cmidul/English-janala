ff1a04f818af14363c96afcb3819ea991bcfe3db
cmidul <mohammadsamidulislam58@gmail.com>
2026-09-07 20:40:29 +0600

    fetching lessons through api in the dash board is applied

--- script.js
+++ script.js
@@ -11,3 +11,22 @@ submit.addEventListener('click', function(){
         document.querySelector('#dashboard').hidden = false;
     }
 })
+
+async function loadLessons() {
+    const response = await fetch(
+      "https://openapi.programming-hero.com/api/levels/all"
+    );
+
+    const data = await response.json()
+    const lessons = data.data
+    const container = document.getElementById('lesson-container');
+    container.innerHTML = '';
+
+    lessons.forEach(lesson => {
+        container.innerHTML += `<button data-level="${lesson.level_no}" class="border flex justify-center items-center gap-[10px] rounded-[5px] text-blue-500 font-semibold hover:bg-blue-600 hover:text-white w-[100px] h-[40px] "><img class="size-[15px]" src="images/fa-book-open.png" alt="">Lesson ${lesson.level_no}</button>`;
+    });
+    
+}
+
+loadLessons();
+
